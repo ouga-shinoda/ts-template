9ca4fef89cdf2f7672df04020763fa8053e1cd3d
ouga-shinoda <ougashinoda@gmail.com>
2025-09-22 08:33:08 +0900

    Update .eslintrc.cjs

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,8 +1,26 @@
 module.exports = {
   root: true,
   env: { es2022: true, node: true },
-  parserOptions: { ecmaVersion: "latest", sourceType: "module" },
-  extends: ["eslint:recommended", "plugin:import/recommended", "prettier"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: { ecmaVersion: "latest", sourceType: "module", project: false },
+  plugins: ["@typescript-eslint", "import"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:import/recommended",
+    "plugin:import/typescript",
+    "prettier",
+  ],
   settings: { "import/resolver": { node: true } },
-  rules: { "import/order": ["warn", { "newlines-between": "always" }] }
+  rules: {
+    "import/order": ["warn", { "newlines-between": "always" }],
+  },
+  overrides: [
+    {
+      files: ["**/*.ts"],
+      rules: {
+        "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+      },
+    },
+  ],
 };
